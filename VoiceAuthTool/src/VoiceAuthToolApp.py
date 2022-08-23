bbed9fd41b353fe9b2914d17852d1b9e4f99ad2b
Francisco Basili <57078209+FranBasili@users.noreply.github.com>
2022-08-22 23:55:11 -0300

    Revert "Merge branch 'master' of https://github.com/sergioandyp/ASSD-TP4"
    This reverts commit b4f034b6aa37c021175de5850a9d90e2b1bfd254, reversing
    changes made to 0580b22dc99f59c9b5f0df9dcc40f896c0bb8749.

--- VoiceAuthTool/src/VoiceAuthToolApp.py
+++ VoiceAuthTool/src/VoiceAuthToolApp.py
@@ -14,12 +14,23 @@ class VoiceAuthToolApp(QMainWindow, Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
 
-        self.main_listwidget.clear()
+        # device = QAudioDeviceInfo(QAudioDeviceInfo.defaultOutputDevice())
+
+        # print(device.supportedCodecs())
+        # print(device.supportedChannelCounts())
+        # print(device.supportedByteOrders())
+        # print(device.supportedSampleSizes())
+        # print(device.supportedSampleRates())
 
         self.horizontalWidget_32.setVisible(False)
         self.horizontalWidget_9.setVisible(False)
+        
 
+        # self.selection1_listwidget.itemEntered.connect(lambda x: print("Item entered"))
         self.selection1_listwidget.itemChanged.connect(self.audioDragged1)
+        # self.selection1_listwidget.currentItemChanged.connect(lambda x, y: print("Current Item Changed"))
+        # self.selection1_listwidget.currentRowChanged.connect(lambda x: print("Current Item Changed"))
+        
         self.selection2_listwidget.itemChanged.connect(self.audioDragged2)
 
         self.add_btn.clicked.connect(self.addAudioFile)
@@ -28,9 +39,7 @@ class VoiceAuthToolApp(QMainWindow, Ui_MainWindow):
         self.compare_btn.clicked.connect(self.compare)
 
         self.player1 = QMediaPlayer()
-        self.player1.setNotifyInterval(100)
         self.player2 = QMediaPlayer()
-        self.player2.setNotifyInterval(100)
         
         self.player1.positionChanged.connect(self.selection1_slider.setValue)
         self.player2.positionChanged.connect(self.selection2_slider.setValue)
@@ -41,8 +50,8 @@ class VoiceAuthToolApp(QMainWindow, Ui_MainWindow):
         self.player1.stateChanged.connect(self.player1StateChange)
         self.player2.stateChanged.connect(self.player2StateChange)
 
-        self.selection1_slider.sliderMoved.connect(self.player1.setPosition)    # Solo se modifican cuando el usuario
-        self.selection2_slider.sliderMoved.connect(self.player2.setPosition)    # mueve el slider (para evitar loop con el player)
+        # self.selection1_slider.valueChanged.connect(self.player1.setPosition)
+        # self.selection2_slider.valueChanged.connect(self.player2.setPosition)
 
         self.selection1_btn.clicked.connect(self.playPause1)
         self.selection2_btn.clicked.connect(self.playPause2)
@@ -50,7 +59,7 @@ class VoiceAuthToolApp(QMainWindow, Ui_MainWindow):
         self.playIcon = [QIcon("res/play.png"), QIcon("res/pause.png")]
         self.recordIcon = [QIcon("res\icons8-add-record-100.png"), QIcon("res/recording.png")]
 
-        self.selection1 = None  # Audios seleccionados para comparar
+        self.selection1 = None
         self.selection2 = None
 
         # Formato de audio
@@ -68,7 +77,7 @@ class VoiceAuthToolApp(QMainWindow, Ui_MainWindow):
         self.encoder.setCodec("audio/amr")
         self.encoder.setQuality(QMultimedia.HighQuality)
 
-        self.recorder = QAudioRecorder()    # Grabador de audio
+        self.recorder = QAudioRecorder()
 
         self.recorder.stateChanged.connect(lambda state: self.record_btn.setIcon(self.recordIcon[state == QAudioRecorder.RecordingState]))
 
@@ -90,6 +99,7 @@ class VoiceAuthToolApp(QMainWindow, Ui_MainWindow):
 
             except Exception as e:
                 QMessageBox.warning(self, "Error al cargar el archivo", str(e))
+                # print("Error al cargar el archivo: ", e)
 
     def addAudio(self, audio):
         self.main_listwidget.addItem(self.makeAudioItem(audio))
@@ -107,6 +117,8 @@ class VoiceAuthToolApp(QMainWindow, Ui_MainWindow):
         font.setPointSize(10)
         item.setFont(font)
         item.setFlags(QtCore.Qt.ItemIsSelectable|QtCore.Qt.ItemIsEditable|QtCore.Qt.ItemIsDragEnabled|QtCore.Qt.ItemIsDropEnabled|QtCore.Qt.ItemIsUserCheckable|QtCore.Qt.ItemIsEnabled)
+        
+        # print("Guardo:", item.data(QtCore.Qt.UserRole))
 
         return item
 
@@ -114,7 +126,8 @@ class VoiceAuthToolApp(QMainWindow, Ui_MainWindow):
 
         self.horizontalWidget_32.setVisible(False)
         self.horizontalWidget_9.setVisible(False)
-        self.compare_btn.setEnabled(True)
+
+        # print("Audio 1 Dragged")
 
         if item.data(QtCore.Qt.UserRole):   # Solo si llego la data
             self.selection1 = item.data(QtCore.Qt.UserRole)   # Guardo el audio
@@ -123,17 +136,77 @@ class VoiceAuthToolApp(QMainWindow, Ui_MainWindow):
             self.selection1_listwidget.takeItem(index)   # Quita el item de la lista
             self.selection1_listwidget.clear()                  # Borra toda la lista
             self.selection1_listwidget.addItem(item)           # Solo agrego este item
+            # print("Recibo1:", item.data(QtCore.Qt.UserRole))
 
             # Carga del audio al player
+
+            # self.buffer = QtCore.QByteArray.fromRawData(self.selection1.tensor.numpy())
+            # self.buffer = QtCore.QBuffer(QtCore.QByteArray.fromRawData(self.selection1.tensor.numpy()))
+            # self.buffer.open(QtCore.QIODevice.ReadOnly)
+            # print(self.buffer.size(), self.buffer.readData(self.buffer.size()))
+
+
+            # self.player1.setMedia(QMediaContent(QtCore.QUrl.fromEncoded(self.buffer)), self.buffer2)
+
+            # self.player1.setMedia(QMediaContent(QtCore.QUrl.fromEncoded(self.buffer)))
+            
+            # content = QMediaContent(QtCore.QUrl.fromLocalFile('D:/Electronica/ASSD/TP4/ASSD-TP4/audio_samples/Basili-1.wav'))
+
+            # self.content = QMediaContent(QtCore.QUrl.fromEncoded(self.buffer))
+            
+            # format = QAudioFormat()
+            # format.setSampleRate(16000)
+            # format.setChannelCount(1)
+            # format.setSampleSize(32)
+            # # format.setCodec("audio/pcm")
+            # format.setByteOrder(QAudioFormat.LittleEndian)
+            # format.setSampleType(QAudioFormat.Float)
+
+            # self.audiobuffer = QAudioBuffer(self.buffer, format)
+            # print("Valid:", self.audiobuffer.isValid())
+
+
+            # device = QAudioDeviceInfo.defaultOutputDevice()
+            # if (device.isFormatSupported(format)):
+            #     print("Formato soportado")
+            # else:
+            #     print("Raw audio format not supported by backend, cannot play audio.")
+
+
+            # self.output = QAudioOutput(format)
+            # self.output.start(self.buffer)
+            # self.player1.setMedia(self.output, self.buffer)
+
+            # self.player1.setMedia(self.content, self.buffer)
+            
+
+            ########### ANDA:
             self.player1.setMedia(QMediaContent(QtCore.QUrl.fromLocalFile(self.selection1.path)))
             self.player1.setVolume(100)
 
+            # print("Audio cargado")
+
+
+        # clone = item.clone()
+        # print("Clon:", clone.data(QtCore.Qt.UserRole))
+
+        # self.selection1_listwidget.clear()  # Borra todo lo que habia
+        # self.selection1_listwidget.addItem(clone)   # Agrega el nuevo item
+
+        # for i in reversed(range(self.selection1_listwidget.count())):
+        #     temp = self.selection1_listwidget.item(i)
+        #     if temp != item:
+        #         self.selection1.takeItem(i)
+        #         # item.
+
+        # print("Audio 1 Dragged")
+        # audio = item.data(QtCore.Qt.UserRole)   # Obtiene el objeto Audio del item
+        # print("Recibo:", audio)
+        # self.selection1 = audio
 
     def audioDragged2(self, item):
 
-        self.horizontalWidget_32.setVisible(False)
-        self.horizontalWidget_9.setVisible(False)
-        self.compare_btn.setEnabled(True)
+        # print("Audio 2 Dragged")
 
         if item.data(QtCore.Qt.UserRole):   # Solo si llego la data
             self.selection2 = item.data(QtCore.Qt.UserRole)   # Guardo el audio
@@ -142,19 +215,21 @@ class VoiceAuthToolApp(QMainWindow, Ui_MainWindow):
             self.selection2_listwidget.takeItem(index)   # Quita el item de la lista
             self.selection2_listwidget.clear()                  # Borra toda la lista
             self.selection2_listwidget.addItem(item)           # Solo agrego este item
+            # print("Recibo2:", item.data(QtCore.Qt.UserRole))
             
             self.player2.setMedia(QMediaContent(QtCore.QUrl.fromLocalFile(self.selection2.path)))
             self.player2.setVolume(100)
+
+        self.horizontalWidget_32.setVisible(False)
+        self.horizontalWidget_9.setVisible(False)
 
 
     def compare(self):
 
         if self.selection1 and self.selection2:
+            score, result = verifySpeaker(self.selection1.tensor, self.selection2.tensor)
 
-            self.compare_btn.setEnabled(False)
-
-            score, result = verifySpeaker(self.selection1.tensor, self.selection2.tensor)   # Se realiza la comparacion
-
+            # print("Resultado:", result)
             self.similarity_number_label.setText(str(round(score, 2)))
 
             if result:
@@ -164,12 +239,12 @@ class VoiceAuthToolApp(QMainWindow, Ui_MainWindow):
 
             self.horizontalWidget_32.setVisible(True)
             self.horizontalWidget_9.setVisible(True)
-            self.compare_btn.setEnabled(True)
-
         else:
             QMessageBox.warning(self, "Error al comparar", "No se seleccionaron audios para comparar")
+            # print("No se seleccionaron audios para comparar")
 
     def player1StateChange(self, state):
+        # print("Player 1 State Change:", state)
         if state == QMediaPlayer.PlayingState:       # Si esta reproduciendo
             self.selection1_btn.setIcon(self.playIcon[1])
         else:                                       # Si esta pausado o detenido
@@ -178,6 +253,7 @@ class VoiceAuthToolApp(QMainWindow, Ui_MainWindow):
                 self.player1.setPosition(0)
 
     def player2StateChange(self, state):
+        # print("Player 2 State Change:", state)
         if state == QMediaPlayer.PlayingState:       # Si esta reproduciendo
             self.selection2_btn.setIcon(self.playIcon[1])
         else:                                       # Si esta pausado o detenido
@@ -189,8 +265,10 @@ class VoiceAuthToolApp(QMainWindow, Ui_MainWindow):
         if self.selection1:
             if self.player1.state() == QMediaPlayer.PlayingState:
                 self.player1.pause()
+                # self.selection1_btn.setIcon(self.playIcon[0])
             else:
                 self.player1.play()
+                # self.selection1_btn.setIcon(self.playIcon[1])
 
     def playPause2(self):
         if self.selection2:
@@ -202,14 +280,13 @@ class VoiceAuthToolApp(QMainWindow, Ui_MainWindow):
     def recordAudio(self):
         if self.recorder.state() == QMediaRecorder.RecordingState:
             self.recorder.stop()
-            print(self.recPath)
-            name = os.path.basename(self.recPath).split(".")[0]
-            audio = Audio(name, loadFile(self.recPath), self.recPath)
-            self.addAudio(audio)
+            # print("Fin de grabación")
         else:
-            self.recPath,_ = QFileDialog.getSaveFileName(self, "Grabar Audio", "", "WAV File (*.wav)")
-            if self.recPath:
+            filename,_ = QFileDialog.getSaveFileName(self, "Grabar Audio", "", "WAV File (*.wav)")
+            if filename:
                 self.recorder.setAudioSettings(self.encoder)
-                self.recorder.setOutputLocation(QtCore.QUrl.fromLocalFile(self.recPath))
+                self.recorder.setOutputLocation(QtCore.QUrl.fromLocalFile(filename))
                 self.recorder.record()
+                # print("Grabando")
+
 
